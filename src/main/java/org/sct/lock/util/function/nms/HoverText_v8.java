0105c0749a976214c21f68150b7879449dcff867
LovesAsuna <qq625924077@gmail.com>
2021-03-16 23:39:37 +0800

    fix(nms): 小更新以支持1.8-1.15版本(未经测试)

---
+++ src/main/java/org/sct/lock/util/function/nms/HoverText_v8.java
@@ -0,0 +1,70 @@
+package org.sct.lock.util.function.nms;
+
+import org.bukkit.Location;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+/**
+ * @author LovesAsuna
+ * @date 2020/3/22 14:30
+ */
+
+public class HoverText_v8 extends AbstractHoverText {
+    private Constructor ChatHoverableConstructor;
+    private Object EnumHoverActionShowText;
+    Method b;
+
+    public HoverText_v8() {
+        super();
+        try {
+            b = ChatHoverable.getDeclaredMethod("b");
+        } catch (ReflectiveOperationException  e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void saveText(Location location, String text) {
+        try {
+            Object ChatModifier = getChatModifier(location);
+
+            if (ChatModifier == null) {
+                // 不是牌子
+                return;
+            }
+            Object ChatComponentText = ChatComponentTextConstructor.newInstance(text);
+            Object ChatHoverable = ChatHoverableConstructor.newInstance(EnumHoverActionShowText, ChatComponentText);
+
+            setChatHoverable.invoke(ChatModifier, ChatHoverable);
+        } catch (ReflectiveOperationException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getText(Location location) {
+        try {
+            Object ChatModifier = getChatModifier(location);
+            if (ChatModifier == null) {
+                // 不是牌子
+                return null;
+            }
+
+            Object ChatHoverable = getHoverEvent.invoke(ChatModifier);
+            Object IChatBaseComponent = null;
+            try {
+                IChatBaseComponent = b.invoke(ChatHoverable);
+            } catch (NullPointerException e) {
+                // 牌子上无消息
+                return null;
+            }
+
+            Method getText = this.IChatBaseComponent.getDeclaredMethod("getText");
+            return (String) getText.invoke(IChatBaseComponent);
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
